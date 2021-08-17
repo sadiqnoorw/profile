d7b658bb56d780bb6d845faea370b3e318c08ece
sadiqnoorw <sadiq.pk22.com>
2021-08-18 01:09:47 +0400

    registration is completed

---
+++ admin/src/services/Service.js
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+const apiClient = axios.create({
+  baseURL: `http://127.0.0.1:8000`,
+  withCredentials: false, // This is the default
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
+})
+
+export default {
+  // post registion information here
+  postRegistion(userInf) {
+    return apiClient.post('/register', userInf)
+  }
+}
--- admin/src/views/Register.vue
+++ admin/src/views/Register.vue
@@ -1,15 +1,103 @@
 <template>
     <div class="main-wrapper">
         <div class="auth-wrapper d-flex no-block justify-content-center align-items-center bg-dark">
-           Registration
+            <div class="auth-box bg-dark border-top border-secondary">
+                <div>
+                    <div class="text-center pt-3 pb-3">
+                        <span class="db"><img src="../assets/theme/assets/images/logo.png" alt="logo" /></span>
+                    </div>
+                    <div class="text-center pt-3">
+                        <h3>Register</h3>
+                    </div>
+                    <!-- Form -->
+                    <form @submit.prevent="registration" class="form-horizontal mt-3" action="index.html">
+                        <div class="row pb-4">
+                            <div class="col-12">
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text bg-success text-white h-100" id="basic-addon1"><i class="ti-user"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control form-control-lg" v-model="first_name" placeholder="First Name" aria-label="First Name" aria-describedby="basic-addon1">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text bg-success text-white h-100" id="basic-addon1"><i class="ti-user"></i></span>
+                                    </div>
+                                    <input type="text" class="form-control form-control-lg" v-model="last_name" placeholder="Last Name" aria-label="Last Name" aria-describedby="basic-addon1">
+                                </div>
+                                <!-- email -->
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text bg-danger text-white h-100" id="basic-addon1"><i class="ti-email"></i></span>
+                                    </div>
+                                    <input type="email" class="form-control form-control-lg" v-model="email" placeholder="Email Address" aria-label="Username" aria-describedby="basic-addon1">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text bg-warning text-white h-100" id="basic-addon2"><i class="ti-pencil"></i></span>
+                                    </div>
+                                    <input type="password" class="form-control form-control-lg" v-model="password" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+                                </div>
+                                <div class="input-group mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text bg-info text-white h-100" id="basic-addon2"><i class="ti-pencil"></i></span>
+                                    </div>
+                                    <input type="password" class="form-control form-control-lg" v-model="confirm_password" placeholder=" Confirm Password" aria-label="Password" aria-describedby="basic-addon1">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row border-top border-secondary">
+                            <div class="col-12">
+                                <div class="form-group">
+                                    <div class="pt-3 d-grid">
+                                        <button class="btn btn-block btn-lg btn-info" type="submit">Sign Up</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+    import Service from '@/services/Service.js'
 
     export default {
         name: 'Register',
+        data() {
+            return {
+                first_name : '',
+                last_name: '',
+                email: '',
+                password: '',
+                confirm_password: ''
+            }
+        },
+
+        methods: {
+            async registration() {
+
+                Service.postRegistion({
+                    first_name : this.first_name,
+                    last_name: this.last_name,
+                    email: this.email,
+                    password: this.password,
+                    password_confirm: this.confirm_password
+                })
+                .then( response => {
+                    console.log(response)
+                    this.$router.push('/login')
+                }
+                ).catch(
+                    error => {
+                        console.log(error)
+                    }
+                )
+            }
+        }
     }
 </script>
 
